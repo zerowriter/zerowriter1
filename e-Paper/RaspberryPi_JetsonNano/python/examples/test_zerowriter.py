1365cecd10d3f933f62d59373fc4f92aac68352a
Jacob Smith <jacob.wesley.smith@gmail.com>
2023-12-29 11:33:01 -0500

    add v2 epd

--- e-Paper/RaspberryPi_JetsonNano/python/examples/test_zerowriter.py
+++ e-Paper/RaspberryPi_JetsonNano/python/examples/test_zerowriter.py
@@ -19,7 +19,6 @@ zero_writer.loop()
 
 for letter in list("this is some test text that we can use to test the zerowriter. it should be long enough to wrap around the screen."):
     zero_writer.keyboard.simulate_key_release(MockKeyEvent(letter))
-
-zero_writer.loop()
+    zero_writer.loop()
 
 time.sleep(2)
